fd65900a56e772311e702edb9b01a799f56facf8
haydi64 <44182284+haydi64@users.noreply.github.com>
2019-10-12 18:41:26 +0100

    Add files via upload

---
+++ junit1.java
@@ -0,0 +1,14 @@
+package HaydenPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class junit1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
