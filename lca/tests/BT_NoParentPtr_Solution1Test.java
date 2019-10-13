027a456d65d93af59b5e1f135d35ac0172ab649a
haydi64 <44182284+haydi64@users.noreply.github.com>
2019-10-13 23:12:28 +0100

    Testing a small, simple, symmetric tree. Also, testing the same tree using a parent node and a child to ensure the parent node was still returned as the answer.

--- lca/tests/BT_NoParentPtr_Solution1Test.java
+++ lca/tests/BT_NoParentPtr_Solution1Test.java
@@ -12,5 +12,29 @@ public class BT_NoParentPtr_Solution1Test {
         sample.root = new Node(1);
         Assert.assertEquals("Test one node Tree",1 , sample.findLCA(1, 1));
     }
+    @Test
+    public void testSimpleTree() {
+        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+        Assert.assertEquals(2, tree.findLCA(4,5));
+    }
 
+    @Test
+    public void testSimpleTreeSameNodeIsLCA() {
+        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+        Assert.assertEquals(2, tree.findLCA(2,4));
+    }
 }
