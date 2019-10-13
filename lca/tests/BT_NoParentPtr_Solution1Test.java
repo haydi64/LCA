a032c52212936fcdc930f4624a9fa234dc49a812
haydi64 <44182284+haydi64@users.noreply.github.com>
2019-10-14 00:01:03 +0100

    Test with a long one sided tree

--- lca/tests/BT_NoParentPtr_Solution1Test.java
+++ lca/tests/BT_NoParentPtr_Solution1Test.java
@@ -57,5 +57,24 @@ public class BT_NoParentPtr_Solution1Test {
         Assert.assertEquals(8, tree.findLCA(11,12));
     }
 
+    @Test
+    public void testLongOneBranchedTree() {
+        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
+        tree.root = new Node(1);
+        tree.root.right = new Node(2);
+        tree.root.right.right = new Node(3);
+        tree.root.right.right.right = new Node(4);
+        tree.root.right.right.right.right = new Node(5);
+        tree.root.right.right.right.right.right = new Node(6);
+        tree.root.right.right.right.right.right.right = new Node(7);
+        tree.root.right.right.right.right.right.right.right = new Node(8);
+        tree.root.right.right.right.right.right.right.right.right = new Node(9);
+        tree.root.right.right.right.right.right.right.right.right.right = new Node(10);
+        tree.root.right.right.right.right.right.right.right.right.right.right = new Node(11);
+        tree.root.right.right.right.right.right.right.right.right.right.right.right = new Node(12);
+        Assert.assertEquals(3,tree.findLCA(3,12));
+
+    }
+
 
 }
