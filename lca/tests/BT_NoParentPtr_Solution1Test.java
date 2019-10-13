90041f2965249e81f113908f4b75a665e8ad25ff
haydi64 <44182284+haydi64@users.noreply.github.com>
2019-10-13 23:28:37 +0100

    Test more complex binary tree that is non-symmetric.

--- lca/tests/BT_NoParentPtr_Solution1Test.java
+++ lca/tests/BT_NoParentPtr_Solution1Test.java
@@ -37,4 +37,25 @@ public class BT_NoParentPtr_Solution1Test {
         tree.root.right.right = new Node(7);
         Assert.assertEquals(2, tree.findLCA(2,4));
     }
+
+    @Test
+    public void testComplexTree() {
+        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+        tree.root.right.right.left = new Node(8);
+        tree.root.right.right.left.left = new Node(9);
+        tree.root.right.right.left.left.right = new Node(10);
+        tree.root.right.right.left.left.right.left = new Node(11);
+        tree.root.right.right.left.right = new Node(12);
+
+        Assert.assertEquals(8, tree.findLCA(11,12));
+    }
+
+
 }
