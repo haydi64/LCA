e1d282dbec2742f5ffae5ecac234749ffb610f7c
haydi64 <44182284+haydi64@users.noreply.github.com>
2019-10-14 01:18:13 +0100

    Test using a value and node reference that doesn't exist on the tree.

--- lca/tests/BT_NoParentPtr_Solution1Test.java
+++ lca/tests/BT_NoParentPtr_Solution1Test.java
@@ -101,4 +101,17 @@ public class BT_NoParentPtr_Solution1Test {
 
     }
 
+    @Test
+    public void testValueDoesntExist() {
+        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
+        tree.root = new Node(1);
+        tree.root.right = new Node(2);
+        tree.root.left = new Node(3);
+        tree.root.right.left = new Node(4);
+        tree.root.right.right = new Node(6);
+        tree.root.left.right = new Node(5);
+        tree.root.left.left = new Node(7);
+        Assert.assertEquals(-1,tree.findLCA(3,9));
+
+    }
 }
